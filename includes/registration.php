bebb647d104f518bdae19689056af80557261cb9
Boone B Gorges <boonebgorges@gmail.com>
2017-05-17 22:35:22 -0500

    Allow wildcards to be saved in email domain whitelist.

--- includes/registration.php
+++ includes/registration.php
@@ -1,5 +1,7 @@
 <?php
 
+add_filter( 'sanitize_option_limited_email_domains', 'cboxol_registration_sanitize_limited_email_domains', 10, 3 );
+
 function cboxol_registration_admin_page() {
 	wp_enqueue_script( 'cbox-ol-app' );
 
@@ -33,4 +35,40 @@ function cboxol_registration_admin_page() {
 	<div id="cboxol-admin"></div>
 
 	<?php
+}
+
+/**
+ * Allow wildcard email email domains in the whitelist.
+ *
+ * @param array  $value
+ * @param string $option
+ * @param array  $original_value
+ * @return array
+ */
+function cboxol_registration_sanitize_limited_email_domains( $value, $option, $original_value ) {
+	if ( $value === $original_value ) {
+		return $value;
+	}
+
+	if ( ! is_array( $original_value ) ) {
+		$original_value = explode( "\n", $original_value );
+	}
+
+	$rejected = array_diff( $original_value, $value );
+
+	$has_wildcard = array();
+	foreach ( $rejected as $domain ) {
+		if ( ! preg_match( '/(--|\.\.)/', $domain ) && preg_match( '|^([a-zA-Z0-9-\.\*])+$|', $domain ) ) {
+			$has_wildcard[] = $domain;
+		}
+	}
+
+	if ( $has_wildcard ) {
+		$value = array_merge( $value, $has_wildcard );
+	}
+
+	// Sort (this makes the order consistent in Network Admin).
+	sort( $value );
+
+	return $value;
 }
