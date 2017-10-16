cuny-academic-commons/cbox-openlab-core

commit 5af46031afdf6089f7afc07363149f15b3eeea8d

New groups must have type saved earlier.
In order to be caught by other parts of the group creation process, such as
the "does this group type require a site" step.

See cuny-academic-commons/openlab#64, cuny-academic-commons/openlab#62.

includes/group-types.php
@@ -5,7 +5,7 @@ add_action( 'bp_groups_register_group_types', 'cboxol_grouptypes_register_group_
 
 // Group creation. We roll our own because we skip BP's validation.
 add_action( 'bp_after_group_details_creation_step', 'cboxol_grouptypes_hidden_field' );
-add_action( 'groups_create_group_step_save_group-details', 'cboxol_grouptypes_save_group_type' );
+add_action( 'groups_group_after_save', 'cboxol_grouptypes_save_group_type', 15 );
 
 // Group creation must always have a group_type.
 add_action( 'bp_actions', 'cboxol_enforce_group_type_on_creation' );

commit 65b4f65138b61592755fe558b5ece78dbf95dcbe

Ensure that group type is set properly at group creation.
Broken in 5af46031afdf6089f7afc07363149f15b3eeea8d, where bp_get_new_group_id()
is not yet set.

See #91.

includes/group-types.php
@@ -292,7 +292,7 @@ function cboxol_grouptypes_hidden_field() {
 /**
  * Save group type.
  */
-function cboxol_grouptypes_save_group_type() {
+function cboxol_grouptypes_save_group_type( $group ) {
 	if ( ! isset( $_POST['group-type'] ) ) {
 		return;
 	}
@@ -317,7 +317,7 @@ function cboxol_grouptypes_save_group_type() {
 		return;
 	}
 
-	bp_groups_set_group_type( bp_get_new_group_id(), $group_type->get_slug() );
+	bp_groups_set_group_type( $group->id, $group_type->get_slug() );
 }
 
 /**