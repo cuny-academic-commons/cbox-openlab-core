d1726785543a0bf6606d6b18958911b81bf38f7e
Boone B Gorges <boonebgorges@gmail.com>
2018-04-16 14:36:35 -0500

    More attempts at twentyfifteen sidebar styling.
    See #158.

--- assets/js/themes/twentyfifteen.js
+++ assets/js/themes/twentyfifteen.js
@@ -1,7 +1,30 @@
-(function(){
+(function($){
 	setTimeout(function(){
 		// Necessary to fool Twenty Fifteen's scroll detection.
-		document.getElementById('sidebar').style.position = 'relative';
-	},500);
-	*/
-}())
+		if ( document.getElementById('sidebar').clientHeight > document.getElementById('primary').clientHeight ) {
+			document.getElementById('sidebar').style.position = 'relative';
+
+		} else {
+			document.getElementById('sidebar').style.position = 'relative';
+			document.getElementById('sidebar').style.height = document.getElementById('primary').clientHeight + 'px';
+		console.log(document.getElementById('sidebar').clientHeight);
+		console.log(document.getElementById('primary').clientHeight);
+		}
+	},1000);
+
+	$(document).ready( function() {
+		var $sidebar, $footer;
+		var sidebarBottom, footerTop;
+		$sidebar = $( '#sidebar' ).first();
+		$footer  = $('#openlab-footer');
+
+		$(window).on( 'scroll.twentyfifteen', function() {
+				footerTop = $footer.offset().top;
+				sidebarBottom = $sidebar.offset().top + $sidebar.outerHeight(true);
+
+				if ( sidebarBottom > footerTop ) {
+					$sidebar.css('position','relative');
+				}
+			} );
+		} );
+}(jQuery))
