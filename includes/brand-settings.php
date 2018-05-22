7a9706d95fb2b271a19a9eb0299a52a793356a86
Boone B Gorges <boonebgorges@gmail.com>
2018-05-21 20:16:22 -0500

    Text for Brand Settings panel.
    See cuny-academic-commons/openlab-theme#209.

--- includes/brand-settings.php
+++ includes/brand-settings.php
@@ -21,7 +21,7 @@ function cboxol_brand_admin_page() {
 	<div class="cboxol-admin-content has-top-border">
 		<h3 class="cboxol-admin-content-header"><?php esc_html_e( 'Copy', 'cbox-openlab-core' ); ?></h3>
 		<div class="cboxol-admin-content-copy">
-			<p><?php esc_html_e( 'Change the copy on your About, Help, and other pages here.', 'cbox-openlab-core' ); ?></p>
+			<p><?php esc_html_e( 'View and change the copy on your About, Help, Terms of Use, and other pages through the Edit and Preview links for each page below.', 'cbox-openlab-core' ); ?></p>
 
 			<?php foreach ( $pages as $page_name => $page_info ) : ?>
 				<?php if ( ! empty( $page_info['id'] ) ) : ?>
@@ -43,20 +43,20 @@ function cboxol_get_brand_page_types() {
 	return array(
 		'about' => array(
 			'settings_page_title' => __( 'About Page', 'cbox-openlab-core' ),
-			'settings_page_description' => __( 'Blanditiis quia autem culpa voluptate. Consequuntur ipsum pariatur accusamus porro incidunt ut sint non. Eius alias rem expedita iste. Esse dignissimos fugiat veniam pariatur voluptatibus.', 'cbox-openlab-core' ),
+			'settings_page_description' => __( 'This page can contain an introduction to your site, institution, and/or organization. Consequuntur ipsum pariatur accusamus porro incidunt ut sint non. Eius alias rem expedita iste. Esse dignissimos fugiat veniam pariatur voluptatibus.', 'cbox-openlab-core' ),
 		),
 		'help' => array(
 			'settings_page_title' => __( 'Help Page', 'cbox-openlab-core' ),
-			'settings_page_description' => __( 'Blanditiis quia autem culpa voluptate. Consequuntur ipsum pariatur accusamus porro incidunt ut sint non. Eius alias rem expedita iste. Esse dignissimos fugiat veniam pariatur voluptatibus.', 'cbox-openlab-core' ),
+			'settings_page_description' => __( 'This section can contain help and support documentation and answers to frequently asked questions for your site’s members and visitors.', 'cbox-openlab-core' ),
 		),
 		'terms-of-use' => array(
-			'settings_page_title' => __( 'Terms of Use', 'cbox-openlab-core' ),
-			'settings_page_description' => __( 'Blanditiis quia autem culpa voluptate. Consequuntur ipsum pariatur accusamus porro incidunt ut sint non. Eius alias rem expedita iste. Esse dignissimos fugiat veniam pariatur voluptatibus.', 'cbox-openlab-core' ),
+			'settings_page_title' => __( 'Terms of Service', 'cbox-openlab-core' ),
+			'settings_page_description' => __( 'This page can contain the Terms of Service for your site. Terms of Service are the rules that a user must abide by while using your site.', 'cbox-openlab-core' ),
 			'parent' => 'about',
 		),
 		'contact-us' => array(
 			'settings_page_title' => __( 'Contact Page', 'cbox-openlab-core' ),
-			'settings_page_description' => __( 'Blanditiis quia autem culpa voluptate. Consequuntur ipsum pariatur accusamus porro incidunt ut sint non. Eius alias rem expedita iste. Esse dignissimos fugiat veniam pariatur voluptatibus.', 'cbox-openlab-core' ),
+			'settings_page_description' => __( 'This page can contain contact information for the administrators of your site, which visitors to the site can use when they have questions, comments, or need help.', 'cbox-openlab-core' ),
 			'parent' => 'about',
 		),
 	);
