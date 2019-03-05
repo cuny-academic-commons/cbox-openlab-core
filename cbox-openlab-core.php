9a0dd9e58d763f1205e619e6b18f7a9103e95910
Boone B Gorges <boonebgorges@gmail.com>
2019-03-05 15:45:30 +0000

    1.1.2-alpha.

--- cbox-openlab-core.php
+++ cbox-openlab-core.php
@@ -5,14 +5,14 @@
  * Description:     Core functionality for CBOX-OpenLab
  * Text Domain:     cbox-openlab-core
  * Domain Path:     /languages
- * Version:         1.1.1
+ * Version:         1.1.2-alpha
  * Network:         true
  */
 
 define( 'CBOXOL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CBOXOL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'CBOXOL_PLUGIN_VER', '1.1.1-1551800316365' );
-define( 'CBOXOL_ASSET_VER', '1.1.1-1551800316365' );
+define( 'CBOXOL_PLUGIN_VER', '1.1.2-alpha-1551800724759' );
+define( 'CBOXOL_ASSET_VER', '1.1.2-alpha-1551800724759' );
 
 // @todo Organize this in a meaningful way.
 function cboxol_init() {
