588f7c1f7dbce8ec250268bc0042909a4858996b
Boone B Gorges <boonebgorges@gmail.com>
2020-07-16 14:41:57 -0500

    Group categories should be fetchable without group type.

--- lib/bp-customizable-group-categories/includes/utility/bp-customizable-group-utility-functions.php
+++ lib/bp-customizable-group-categories/includes/utility/bp-customizable-group-utility-functions.php
@@ -5,26 +5,30 @@
  * Fun for the outside world
  */
 
-function bpcgc_get_terms_by_group_type( $group_type ) {
+function bpcgc_get_terms_by_group_type( $group_type = '' ) {
 	global $wpdb;
 
 	$terms_out = array();
 
-	if ( ! $group_type ) {
-		return false;
-	}
+	if ( $group_type ) {
+		$key = 'bpcgc_group_' . $group_type;
 
-	$key = 'bpcgc_group_' . $group_type;
+		//if sorting plugin Category Order and Taxonomy Terms Order is available, use the custom sort
+		if ( function_exists( 'tto_info_box' ) ) {
+			$query = $wpdb->prepare( "SELECT t.* FROM $wpdb->terms t INNER JOIN $wpdb->termmeta tm on tm.term_id = t.term_id WHERE tm.meta_key=%s ORDER BY t.term_order ASC", $key );
+		} else {
+			$query = $wpdb->prepare( "SELECT t.* FROM $wpdb->terms t INNER JOIN $wpdb->termmeta tm on tm.term_id = t.term_id WHERE tm.meta_key=%s", $key );
+		}
 
-	//if sorting plugin Category Order and Taxonomy Terms Order is available, use the custom sort
-	if ( function_exists( 'tto_info_box' ) ) {
-		$query = $wpdb->prepare( "SELECT t.* FROM $wpdb->terms t INNER JOIN $wpdb->termmeta tm on tm.term_id = t.term_id WHERE tm.meta_key=%s ORDER BY t.term_order ASC", $key );
+		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		$terms_out = $wpdb->get_results( $query );
 	} else {
-		$query = $wpdb->prepare( "SELECT t.* FROM $wpdb->terms t INNER JOIN $wpdb->termmeta tm on tm.term_id = t.term_id WHERE tm.meta_key=%s", $key );
+		$terms_out = get_terms(
+			[
+				'taxonomy' => 'bp_group_categories',
+			]
+		);
 	}
-
-	// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
-	$terms_out = $wpdb->get_results( $query );
 
 	return $terms_out;
 }
