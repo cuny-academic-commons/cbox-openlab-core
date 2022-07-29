05a600685c95ef7f9e6c0c9b75b84978f8e14ccc
Boone B Gorges <boonebgorges@gmail.com>
2022-07-29 13:58:38 -0500

    Standardize messaging about site template site selector.
    See cuny-academic-commons/commons-in-a-box#400.

--- views/site-template/admin/template-site.php
+++ views/site-template/admin/template-site.php
@@ -14,16 +14,18 @@ if ( $site_id ) {
 
 <label class="screen-reader-text" for="template-site-id"><?php esc_html_e( 'Template Site', 'cboxol-site-template-picker' ); ?></label>
 
-<p><?php esc_html_e( 'Below you can select an existing site whose settings and content will be copied to new sites when using this template. If no site is selected, a new site will be created automatically to serve as the template.', 'commons-in-a-box' ); ?></p>
+<p class="description">
+	<?php esc_html_e( 'Below you can select an existing site whose settings and content will be copied to new sites when using this template.', 'commons-in-a-box' ); ?>
+
+	<?php if ( $is_create ) : ?>
+		<?php esc_html_e( 'If no site is selected, a new site will be created automatically to serve as the template.', 'commons-in-a-box' ); ?>
+	<?php endif; ?>
+</p>
 
 <select class="widefat" name="template-site-id" id="template-site-id" required>
 	<option value="<?php echo esc_attr( $site_id ); ?>" selected="selected"><?php echo esc_html( $option_name ); ?></option>
 </select>
 
 <p><?php esc_html_e( 'To search, click into the dropdown and begin typing the name or URL of the desired site.', 'commons-in-a-box' ); ?></p>
-
-<?php if ( $is_create ) : ?>
-	<p class="description"><?php esc_html_e( 'You may select an existing site. If no site is selected, a new site will be created automatically to serve as the template.', 'commons-in-a-box' ); ?></p>
-<?php endif; ?>
 
 <?php wp_nonce_field( 'cboxol-template-site', 'cboxol-template-site-nonce', false ); ?>
