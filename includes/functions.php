a3e3e1ee67adb499b0e72f47fc9408ed1c2dd76f
Boone B Gorges <boonebgorges@gmail.com>
2023-01-13 10:30:10 -0600

    Rename `openlab_get_current_filter()` to `cboxol_get_current_filter()`.
    The moving of `openlab_get_current_filter()` to cbox-openlab-core
    was causing a variety of problems when upgrading from 1.3.x to 1.4.0.

--- includes/functions.php
+++ includes/functions.php
@@ -326,90 +326,84 @@ function cboxol_get_main_site_id() {
 
 /**
  * Get the current filter value out of GET parameters.
- *
- * Formerly a part of openlab-theme, it's moved here for better availability during
- * installation. It's wrapped in a `function_exists()` call to avoid fatal errors
- * during upgrade from older versions of openlab-theme.
  */
-if ( ! function_exists( 'openlab_get_current_filter' ) ) {
-	function openlab_get_current_filter( $param ) {
-		$value = '';
+function cboxol_get_current_filter( $param ) {
+	$value = '';
 
-		// phpcs:disable WordPress.Security.NonceVerification.Recommended
-		switch ( $param ) {
-			case 'school':
-				if ( isset( $_GET['school'] ) ) {
-					$value_raw           = wp_unslash( $_GET['school'] );
-					$schools_and_offices = array_merge( openlab_get_school_list(), openlab_get_office_list() );
+	// phpcs:disable WordPress.Security.NonceVerification.Recommended
+	switch ( $param ) {
+		case 'school':
+			if ( isset( $_GET['school'] ) ) {
+				$value_raw           = wp_unslash( $_GET['school'] );
+				$schools_and_offices = array_merge( openlab_get_school_list(), openlab_get_office_list() );
 
-					if ( 'school_all' === $value_raw ) {
-						$value = 'school_all';
-					} elseif ( isset( $schools_and_offices[ $value_raw ] ) ) {
-						$value = $value_raw;
-					}
+				if ( 'school_all' === $value_raw ) {
+					$value = 'school_all';
+				} elseif ( isset( $schools_and_offices[ $value_raw ] ) ) {
+					$value = $value_raw;
 				}
-				break;
+			}
+			break;
 
-			case 'group_types':
-				$value = isset( $_GET['group_types'] ) ? wp_unslash( $_GET['group_types'] ) : [];
-				break;
+		case 'group_types':
+			$value = isset( $_GET['group_types'] ) ? wp_unslash( $_GET['group_types'] ) : [];
+			break;
 
-			case 'usertype':
-				if ( isset( $_GET['usertype'] ) ) {
-					$user_types    = array_merge( openlab_valid_user_types(), [ 'user_type_all' ] );
-					$user_type_raw = wp_unslash( $_GET['usertype'] );
-					if ( in_array( $user_type_raw, $user_types, true ) ) {
-						$value = $user_type_raw;
-					}
+		case 'usertype':
+			if ( isset( $_GET['usertype'] ) ) {
+				$user_types    = array_merge( openlab_valid_user_types(), [ 'user_type_all' ] );
+				$user_type_raw = wp_unslash( $_GET['usertype'] );
+				if ( in_array( $user_type_raw, $user_types, true ) ) {
+					$value = $user_type_raw;
 				}
-				break;
+			}
+			break;
 
-			case 'order':
-				$whitelist = [ 'alphabetical', 'newest', 'active' ];
-				$value     = isset( $_GET['order'] ) && in_array( $_GET['order'], $whitelist, true ) ? $_GET['order'] : 'active';
-				break;
+		case 'order':
+			$whitelist = [ 'alphabetical', 'newest', 'active' ];
+			$value     = isset( $_GET['order'] ) && in_array( $_GET['order'], $whitelist, true ) ? $_GET['order'] : 'active';
+			break;
 
-			case 'open':
-				$value = ! empty( $_GET['is_open'] );
-				break;
+		case 'open':
+			$value = ! empty( $_GET['is_open'] );
+			break;
 
-			case 'cloneable':
-				$value = ! empty( $_GET['is_cloneable'] );
-				break;
+		case 'cloneable':
+			$value = ! empty( $_GET['is_cloneable'] );
+			break;
 
-			case 'badges':
-				$value = isset( $_GET['badges'] ) ? array_map( 'intval', $_GET['badges'] ) : [];
-				break;
+		case 'badges':
+			$value = isset( $_GET['badges'] ) ? array_map( 'intval', $_GET['badges'] ) : [];
+			break;
 
-			case 'group-types':
-				$group_types = isset( $_GET['group-types'] ) ? $_GET['group-types'] : [];
-				$value       = array_filter(
-					wp_unslash( $group_types ),
-					function( $slug ) {
-						$type = cboxol_get_group_type( $slug );
-						return ! is_wp_error( $type );
-					}
-				);
-				break;
-
-			case 'sort':
-				$default = 'active';
-
-				$value = isset( $_GET['sort'] ) ? wp_unslash( $_GET['sort'] ) : $default;
-
-				$allowed = [ 'newest', 'alphabetical', 'active' ];
-				if ( ! in_array( $value, $allowed, true ) ) {
-					$value = $default;
+		case 'group-types':
+			$group_types = isset( $_GET['group-types'] ) ? $_GET['group-types'] : [];
+			$value       = array_filter(
+				wp_unslash( $group_types ),
+				function( $slug ) {
+					$type = cboxol_get_group_type( $slug );
+					return ! is_wp_error( $type );
 				}
+			);
+			break;
 
-				break;
+		case 'sort':
+			$default = 'active';
 
-			default:
-				$value = isset( $_GET[ $param ] ) ? wp_unslash( $_GET[ $param ] ) : '';
-				break;
-		}
-		// phpcs:enable WordPress.Security.NonceVerification.Recommended
+			$value = isset( $_GET['sort'] ) ? wp_unslash( $_GET['sort'] ) : $default;
 
-		return $value;
+			$allowed = [ 'newest', 'alphabetical', 'active' ];
+			if ( ! in_array( $value, $allowed, true ) ) {
+				$value = $default;
+			}
+
+			break;
+
+		default:
+			$value = isset( $_GET[ $param ] ) ? wp_unslash( $_GET[ $param ] ) : '';
+			break;
 	}
+	// phpcs:enable WordPress.Security.NonceVerification.Recommended
+
+	return $value;
 }
