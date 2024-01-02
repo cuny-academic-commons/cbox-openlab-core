780cbdb767f60a51925b95ee4d5a66945bf303d1
Boone B Gorges <boonebgorges@gmail.com>
2024-01-02 12:27:26 -0600

    1.5.1-beta1 version bumps.

--- cbox-openlab-core.php
+++ cbox-openlab-core.php
@@ -5,15 +5,15 @@
  * Description:     Core functionality for CBOX-OpenLab
  * Text Domain:     cbox-openlab-core
  * Domain Path:     /languages
- * Version:         1.5.0
+ * Version:         1.5.1-beta1
  * Network:         true
  */
 
 define( 'CBOXOL_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'CBOXOL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CBOXOL_PLUGIN_ROOT_FILE', __FILE__ );
-define( 'CBOXOL_PLUGIN_VER', '1.5.0-1691676685352' );
-define( 'CBOXOL_ASSET_VER', '1.5.0-1691676685352' );
+define( 'CBOXOL_PLUGIN_VER', '1.5.1-beta1-1704220040123' );
+define( 'CBOXOL_ASSET_VER', '1.5.1-beta1-1704220040123' );
 
 // @todo Organize this in a meaningful way.
 function cboxol_init() {
