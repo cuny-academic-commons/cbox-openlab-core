e02ceb0235d79ef55bcbf08aec1ca945f2e3f9a3
Boone B Gorges <boonebgorges@gmail.com>
2024-06-24 17:45:19 -0500

    Asset build.

--- build/blocks.asset.php
+++ build/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '3f519e26cf884caecd34');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '51201e44a3f25ae7203e');
