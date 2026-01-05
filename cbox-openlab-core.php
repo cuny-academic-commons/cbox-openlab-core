e0159f21ee1651f8eb4c6f2827f1d3d772cf4ad2
Boone B Gorges <boonebgorges@gmail.com>
2026-01-05 10:37:36 -0600

    1.7.1-beta1 version bumps.

--- cbox-openlab-core.php
+++ cbox-openlab-core.php
@@ -5,7 +5,7 @@
  * Description:     Core functionality for CBOX-OpenLab
  * Text Domain:     cbox-openlab-core
  * Domain Path:     /languages
- * Version:         1.7.0
+ * Version:         1.7.1-beta1
  * Network:         true
  */
 
