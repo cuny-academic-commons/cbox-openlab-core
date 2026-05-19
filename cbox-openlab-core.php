3eff955ef144485bd35bda9d5213c779ee81966a
Boone B Gorges <boonebgorges@gmail.com>
2026-05-19 13:50:58 -0500

    Remove AI-crawlers feature.
    See cuny-academic-commons/commons-in-a-box#560.

--- cbox-openlab-core.php
+++ cbox-openlab-core.php
@@ -53,7 +53,6 @@ function cboxol_init() {
 	require CBOXOL_PLUGIN_DIR . 'includes/registration.php';
 	require CBOXOL_PLUGIN_DIR . 'includes/communication-settings.php';
 	require CBOXOL_PLUGIN_DIR . 'includes/profile-fields.php';
-	require CBOXOL_PLUGIN_DIR . 'includes/robots.php';
 	require CBOXOL_PLUGIN_DIR . 'includes/search.php';
 	require CBOXOL_PLUGIN_DIR . 'includes/site-template.php';
 
